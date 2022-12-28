18eb03c84ffd9efbe18108dd655fe41d60910872
Azzam Mahmoud <azzammahmoud007@gmail.com>
2022-12-28 11:50:38 +0200

    step3

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
-
+let date = new Date()
+let localTime = date.toLocaleTimeString()
 app.get('/',  (req, res) => {
   res.send('ok')
 })
-
+app.get('/test',(req,res)=>{
+    res.status(200)
+    res.send({message:`ok`})
+})
+app.get('/time', (req, res) =>{
+    res.status(200)
+    res.send({message:`${localTime}`})
+});
 app.listen(4000);
