cedd61655171f1b2e23389a7b47835ebd26e0649
Azzam Mahmoud <azzammahmoud007@gmail.com>
2022-12-28 12:54:22 +0200

    step4

--- index.js
+++ index.js
@@ -13,4 +13,19 @@ app.get('/time', (req, res) =>{
     res.status(200)
     res.send({message:`${localTime}`})
 });
+app.get(['/hello/:id','/hello'], (req, res) =>{
+    if(req.params.id == undefined){
+        res.send("hello")
+    }else{
+    res.status(200)
+    res.send('hello ' + req.params.id);}
+
+});
+app.get("/search",(req,res)=>{
+    if(typeof req.query.s =="undefined" || req.query.s === "") 
+    {res.send({status:500, error:true, message:"you have to provide a search"})
+    }else {
+        res.send( {status:200, message:"ok", data:req.query.s})
+    }
+})
 app.listen(4000);
